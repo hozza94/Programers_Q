7e331164b656772b1dca3a8e8c54511dd70e13d2
hozza94 <hozza94@users.noreply.github.com>
2020-09-19 22:29:02 +0900

    Create DP_정수삼각형.py

---
+++ DP_정수삼각형.py
@@ -0,0 +1,21 @@
+def solution(triangle):
+    answer = 0
+    length = len(triangle)
+
+    W = [[0]*length for _ in range(length)]
+    W[0][0] = triangle[0][0] # 시작 숫자
+
+    for i in range(0,length):
+        for j in range(0,i+1):
+            if j == 0:
+                W[i][j] = W[i-1][j] + triangle[i][j]
+            else:
+                W[i][j] = max(W[i-1][j-1] + triangle[i][j],
+                                  W[i-1][j] + triangle[i][j])
+    answer = max(W[length-1])
+    return answer
+
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+result = 30
+
+print(solution(triangle))
