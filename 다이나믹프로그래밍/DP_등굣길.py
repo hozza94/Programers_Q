3b4fbfa516817044423f1c44ade7879ee3254b5e
hozza94 <hozza94@users.noreply.github.com>
2021-04-04 20:45:36 +0900

    Update DP_등굣길.py

--- 다이나믹프로그래밍/DP_등굣길.py
+++ 다이나믹프로그래밍/DP_등굣길.py
@@ -20,28 +20,15 @@ def solution(m, n, puddles):
 
     for i in range(n, 0, -1):
         for j in range(m, 0, -1):
-            if W[i][j] == 0:
+            if W[i][j] == 0:  # 아래 테두리에선 오른쪽 값만, 오른쪽 테두리에선 아랫값만 신경써주면 된다
                 if j == m:
-                    P[i][j] = P[i+1][j] % 1000000007
+                    P[i][j] = P[i + 1][j] % 1000000007
                 elif i == n:
-                    P[i][j] = P[i][j+1] % 1000000007
+                    P[i][j] = P[i][j + 1] % 1000000007
                 else:
-                    P[i][j] = P[i][j+1] + P[i+1][j] % 1000000007
+                    P[i][j] = P[i][j + 1] + P[i + 1][j] % 1000000007
 
     print(P)
-
-    # for i in range(n, 0, -1):
-    #     for j in range(m, 0, -1):
-    #         if W[j - 1][i] < 0:
-    #             if W[j][i - 1] < 0:
-    #                 if j == 1 and i == 1:
-    #                     # 정점 도착
-    #                     print("ok")
-    #                 else:
-    #                     print("end")
-    #             else:
-    #                 P[j][i - 1] += 1
-
     print(W)
     return P[1][1] % 1000000007
 
@@ -55,9 +42,8 @@ def solution(m, n, puddles):
 # puddles = [[2, 2]]
 # result = 4
 
-m = 2
-n = 2
-puddles = []
-result = 2
+m = 5
+n = 4
+puddles = [[2, 1], [2, 2], [2, 3], [4, 2], [4, 3], [4, 4]]
 
 print(solution(m, n, puddles))
