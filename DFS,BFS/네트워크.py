88ec148aef2eda56f0ef3d813c23c96306ab8c35
hozza94 <hozza94@users.noreply.github.com>
2021-04-04 20:37:34 +0900

    Update 네트워크.py

--- DFS,BFS/네트워크.py
+++ DFS,BFS/네트워크.py
@@ -1,40 +1,38 @@
 def solution(n, computers):
     answer = 0
     visit = [0] * len(computers)
+    st = []
 
-    def dfs(s):
-        st = [s]
-
-        while st:
-            num = st.pop()
-            if visit[num] == 0:
-                visit[num] = 1
-            for i in range(len(computers[0])):
-                if computers[num][i] == 1 and visit[i] == 0:
-                    st.append(i)
-    i = 0
-    while 0 in visit:
+    for i in range(len(computers)):
         if visit[i] == 0:
-            dfs(i)
-            answer += 1
-        i += 1
+            visit[i] = 1
+            for j in range(i+1, len(computers)):
+                if computers[i][j] == 1:
+                    st.append(j)
+        else:
+            pass
 
+    print(st)
     return answer
-
-
-
 
 
 # n	computers	return
 # 3	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]	2
 # 3	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]	1
-n = 6
-computers = [[1, 1, 0, 0, 0, 0],
-             [1, 1, 1, 0, 0, 0],
-             [0, 1, 1, 1, 0, 0],
-             [0, 0, 1, 1, 1, 0],
-             [0, 0, 0, 1, 1, 1],
-             [0, 0, 0, 0, 1, 1]]
+
+n = 3
+computers = [[1, 1, 0],
+             [1, 1, 0],
+             [0, 0, 1]]
 result = 2
+
+# n = 6
+# computers = [[1, 1, 0, 0, 0, 0],
+#              [1, 1, 1, 0, 0, 0],
+#              [0, 1, 1, 1, 0, 0],
+#              [0, 0, 1, 1, 1, 0],
+#              [0, 0, 0, 1, 1, 1],
+#              [0, 0, 0, 0, 1, 1]]
+# result = 2
 
 print(solution(n, computers))
