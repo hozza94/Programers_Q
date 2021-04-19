6846bdde6967e7703014625ec4007a487cf2cd4c
hozza94 <hozza94@users.noreply.github.com>
2021-04-19 11:21:04 +0900

    Update H-index.py

--- 정렬/H-index.py
+++ 정렬/H-index.py
@@ -1,25 +1,25 @@
+# h-index <= n
+
 def solution(citations):
     answer = 0
-    count = 0
-    prev = 0
     n = len(citations)
+    citations.sort()
 
-    citations.sort(reverse=True)
-
-    for n in range(len(citations)):
-        prev = n
-        if citations[n] <= 0:
-            break
-
-        if n + 1 >= citations[n]:
-            if n + 1 == citations[n]:
-                answer = n + 1
-            elif prev == citations[n]:
-                answer = prev
+    for i in range(n):
+        if citations[i] >= n - i:
+            answer = max(answer, n-i)
 
     return answer
 
 
 citations = [3, 0, 6, 1, 5]
+
+print(solution(citations))
+
+citations = [4, 4, 4, 5, 0, 1, 2, 3]
+
+print(solution(citations))
+
+citations = [0, 0, 1, 1]
 
 print(solution(citations))
