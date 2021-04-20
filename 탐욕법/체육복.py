2c5d8ad627f4b4df3a0343a0af80c386196eb330
hozza94 <hozza94@users.noreply.github.com>
2021-04-20 12:05:26 +0900

    Update 체육복.py

--- 탐욕법/체육복.py
+++ 탐욕법/체육복.py
@@ -1,24 +1,19 @@
 def solution(n, lost, reserve):
-    student = [1] * n
+    for i in range(1, n + 1):
+        if i in reserve and i in lost:
+            reserve.remove(i)
+            lost.remove(i)
 
-    for l in lost:
-        student[l-1] -= 1
+    for i in range(1, n + 1):
+        if i in reserve:
+            if i - 1 in lost:
+                lost.remove(i - 1)
+                reserve.remove(i)
+            elif i + 1 in lost:
+                lost.remove(i + 1)
+                reserve.remove(i)
 
-    for r in reserve:
-        student[r-1] += 1
-
-    for i in range(n):
-        if student[i] > 1:
-            if student[i-1] < 1:
-                student[i-1] += 1
-            elif student[i+1] < 1:
-                student[i+1] += 1
-            student[i] -= 1
-
-
-    print(student)
-
-    return n - student.count(0)
+    return n - len(lost)
 
 
 n = 5
@@ -28,14 +23,16 @@ result = 5
 
 print(solution(n, lost, reserve))
 
-n = 5
-lost = [2, 4]
-reserve = [3]
-result = 4
+n = 4
+lost = [3, 2, 1]
+reserve = [2, 4, 3]
+result = 3
 print(solution(n, lost, reserve))
 
-n = 4
-lost = [3, 1]
-reserve = [2, 4]
-result = 4
+n = 3
+lost = [1, 2]
+reserve = [2, 3]
+result = 2
 print(solution(n, lost, reserve))
+
+# 12번 : 3 / [1,2] / [2,3] => 답 : 2
