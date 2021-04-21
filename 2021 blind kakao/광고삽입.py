dccf5376bfd3c8e026d7f098808b3448c8ec65ea
hozza94 <hozza94@users.noreply.github.com>
2021-04-21 17:26:58 +0900

    Create 광고삽입.py

---
+++ 2021 blind kakao/광고삽입.py
@@ -0,0 +1,18 @@
+def solution(play_time, adv_time, logs):
+    if play_time == adv_time:
+        return "00:00:00"
+
+
+    answer = ''
+
+    logs.sort()
+    print(logs)
+    return answer
+
+
+play_time = "02:03:55"
+adv_time = "00:14:15"
+logs = ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]
+result = "01:30:59"
+
+print(solution(play_time, adv_time, logs))
