2b6ac6d74d43c21067f8f7e30ed3a2afc6dd83bf
hozza94 <hozza94@users.noreply.github.com>
2021-04-21 13:36:46 +0900

    Update q.03.py
    clear

--- 월간코드챌린지2104/q.03.py
+++ 월간코드챌린지2104/q.03.py
@@ -1,63 +1,49 @@
 from collections import defaultdict
+import sys
+sys.setrecursionlimit(300000)
 
 
 def solution(a, edges):
-    answer = 0
-    graph = defaultdict(list)
+    answer = []
+    child_sum = [0] * len(a)
     visit = [0] * len(a)
-    que = []
+    graph = defaultdict(list)
 
-    for i in range(len(a)):
-        if a[i] == 0:
-            visit[i] = 1
+    if sum(a) != 0:
+        return -1
 
     for v1, v2 in edges:
         graph[v1].append(v2)
         graph[v2].append(v1)
 
-    print(visit)
-    print(graph)
+    def sum_dfs(node):
+        score = 0
 
-    for g in graph:
-        if len(graph[g]) == 1 and visit[g] == 0:
-            que.append(g)
+        if visit[node] == 0:
+            visit[node] = 1
 
-    print(que)
-    while que:
-        top = que.pop(0)
-        print(que)
-        if visit[top] == 0:
-            visit[top] = 1
+            for n in graph[node]:
+                if visit[n] == 0:
+                    score += sum_dfs(n)
 
-            q = 0
-            for g in graph[top]:
-                if visit[g] == 0:
-                    q = g
-            print("q: ", q)
+            child_sum[node] = score + a[node]
+            answer.append(abs(score + a[node]))
 
-            for i in range(0, abs(a[top])):
-                if a[top] == 0:
-                    break
+        return child_sum[node]
 
-                if a[top] > 0:
-                    a[top] -= 1
-                    a[q] += 1
-                else:
-                    a[top] += 1
-                    a[q] -= 1
-                answer += 1
+    sum_dfs(0)
 
-            for g in graph[top]:
-                if visit[g] == 0:
-                    que.append(g)
-
-    if answer == 0:
-        return -1
-    return answer
+    return sum(answer)
 
 
 a = [-5, 0, 2, 1, 2]
 edges = [[0, 1], [3, 4], [2, 3], [0, 3]]
 result = 9
+
+print(solution(a, edges))
+
+a = [0, 1, 0]
+edges = [[0, 1], [1, 2]]
+result = -1
 
 print(solution(a, edges))
