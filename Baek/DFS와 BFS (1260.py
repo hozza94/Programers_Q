3a4683135dfc9e01dc3cd9d05fc39967a050a2c1
hozza94 <hozza94@users.noreply.github.com>
2021-04-22 22:39:50 +0900

    Update DFS와 BFS (1260.py

--- Baek/DFS와 BFS (1260.py
+++ Baek/DFS와 BFS (1260.py
@@ -1,15 +1,6 @@
-# 4 5 1
-# 1 2
-# 1 3
-# 1 4
-# 2 4
-# 3 4
-# 입력
+# https://www.acmicpc.net/problem/1260
+# DFS와 BFS
 
-
-# 1 2 4 3
-# 1 2 3 4
-# 출력
 from collections import defaultdict
 
 v, e, s = map(int, input().split(" "))
