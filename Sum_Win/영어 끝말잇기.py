263049ec18f687702a5cdde4fb6861ec7b80750c
hozza94 <hozza94@users.noreply.github.com>
2021-05-07 16:35:32 +0900

    Update 영어 끝말잇기.py

--- Sum_Win/영어 끝말잇기.py
+++ Sum_Win/영어 끝말잇기.py
@@ -20,3 +20,10 @@ print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "
 print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure",
                    "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]), [0, 0])
 print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]), [1, 3])
+
+# 이건... 참고할만하다
+# def solution(n, words):
+#     for p in range(1, len(words)):
+#         if words[p][0] != words[p-1][-1] or words[p] in words[:p]: return [(p%n)+1, (p//n)+1]
+#     else:
+#         return [0,0]
